7ee9054203ab0c97f8c803444937af9a4f0fd3e8
victorejike <victorejike@gmail.com>
2026-07-17 15:06:47 +0100

    changable

---
+++ week1/replace.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int i = 0;
+    printf("my frist value of interger is : %d\n", i);
+    for (i = 1; i < 20; i++);
+    printf("now the chang value is now : %d \n", i);
+    return 0;
+}
