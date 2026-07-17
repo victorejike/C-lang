fa506e2895fe5bf8c1005950c9a8b0cbbc5d9fb6
victorejike <victorejike@gmail.com>
2026-07-17 14:59:44 +0100

    data type

--- week1/main.c
+++ week1/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    char name[] = "C!";
-    printf("Hello, %s \n", name);
+    int k = 0;
+    char j = '\0';
+    float r = 0.0;
+
+    printf("interger is : %d \n", k);
+    //this is unknow the computer dont kown this but was declaered
+    printf("charater is : %c \n", j);
+    printf("float is  : %0.02f \n", r);
     return 0;
 }
---
+++ week1/variable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+int main(){
+    char name[7] = "victo r";
+    printf("Hello my Name is %s \n", name);
+    int i ;
+    for (i = 0; name[i] != '\0'; i++);
+    printf("the lenght of word is %d \n", i);
+    #what i learned from this project is that you can print an int just with the right formart specifyer
+    printf("my name is victor and i am in clasee %d \n", 6);
+    return 0;
+}
+  
