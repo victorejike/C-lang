fb5b44fe8f4b58a206a166fe7b7e3d1cb91d6932
victorejike <victorejike@gmail.com>
2026-07-17 15:40:47 +0100

    copyin variable in c

---
+++ week1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var myname string = "hello"
+	myname = "victor"
+	fmt.Println(myname)
+
+}
--- week1/replace.c
+++ week1/replace.c
@@ -5,5 +5,22 @@ int main(){
     printf("my frist value of interger is : %d\n", i);
     for (i = 1; i < 20; i++);
     printf("now the chang value is now : %d \n", i);
+    int fristv = 30;
+    printf("my first value is : %d \n", fristv);
+    int secondv = 60;
+    printf("my second value is : %d \n", secondv);
+    fristv = secondv;
+    printf("what is the real value %d\n",fristv);
+
+    int myThrid;
+
+    myThrid = secondv;
+    printf("now the copyed value is now : %d\n", myThrid);
+     
+    int sum = fristv + secondv + myThrid;
+    sum = sum + 1;
+    printf("the total sum of both is : %d \n", sum);
+
+
     return 0;
 }
