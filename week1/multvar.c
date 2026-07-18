6487da1e8957124263caff95ab3152c8a1420fab
victorejike <victorejike@gmail.com>
2026-07-18 14:36:04 +0100

    c program

--- week1/multvar.c
+++ week1/multvar.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    int x = 30;
-    int y = 40; 
-    int z = 60;
-    printf("the multi variable declareation %d, %d, %d \n", x,y,z);
+    // now i want to calculate the area of an rectangle 
+    int  lenght = 60;
+    int  width  = 40;
+    int  area;
+
+    area = lenght + width;
+    printf("the lenght  is %d \n",lenght);
+    printf("the width   is %d \n", width);
+    printf("the total of the angle is %d \n",area);
     return 0;
 }
