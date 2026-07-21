3fcfd0d6206e5a9ba4d84c338dce4336a8643b02
victorejike <victorejike@gmail.com>
2026-07-21 10:29:51 +0100

    clang

---
+++ week1/hash.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+int hashAarry[SIZE] = {0};
+
+int hashcode(int key){
+    return key % SIZE;
+}
+
+//the function that stores the data
+void insert(int key, int value){
+    
+}
